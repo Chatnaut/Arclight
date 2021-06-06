d358ad939890fe0c811922d4e7f1f1c6c45b35b9
s4nfs <zyzzaestheticsgods@gmail.com>
2021-06-06 23:00:41 +0530

    added gpubinder

---
+++ pages/gpu/gpubinder.php
@@ -0,0 +1,114 @@
+
+<?php
+// If the SESSION has not been started, start it now
+if (!isset($_SESSION)) {
+  session_start();
+}
+
+// If there is no username, then we need to send them to the login
+if (!isset($_SESSION['username'])){
+  $_SESSION['return_location'] = $_SERVER['PHP_SELF']; //sets the return location used on login page
+  header('Location: ../login.php');
+}
+require('../header.php');
+require('../navbar.php');
+require('../footer.php');
+
+?>
+
+
+
+<main role="main" class="col-md-9 ml-sm-auto col-lg-10 pt-3 px-4 <?php if($_SESSION['themeColor'] == "dark-edition") { echo "main-dark"; } ?> ">
+
+      <form action="" method="POST">
+        <div class="content">
+          <div class="row">
+
+            <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12">
+
+              <div class="card <?php if($_SESSION['themeColor'] == "dark-edition") { echo "card-dark"; } ?>">
+                <div class="card-header">
+                  <span class="card-title"></span>
+                </div>
+                <div class="card-body">
+
+                  <div class="table-responsive">
+                    <table class="table">
+                      <thead class="text-none">
+                        <th>Physical GPUs [PCI]</th>
+                      </thead>
+                      <tbody>
+                    <!-- start project list -->
+                    <?php
+                    $result = shell_exec('cd /var/www/html/arclight/gpubinder && ./nvidia-dev-ctl.py list-pci -o table');
+
+                      echo "<tr>" .
+                        "<pre><td><strong>{$result}</strong></td></pre>" .
+                        "</tr>";
+                    
+                    echo "</tbody></table>";
+                    ?>
+                  </div>
+                </div>
+              </div>
+
+            </div>
+          </div>
+        </div>
+      </form>
+    </main>
+  </div> 
+</div> <!-- end content of physical GPUs-->
+
+
+
+
+<main role="main" class="col-md-9 ml-sm-auto col-lg-10 pt-3 px-4 <?php if($_SESSION['themeColor'] == "dark-edition") { echo "main-dark"; } ?> ">
+
+    <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pb-2 mb-3">
+        <h3 class="h3">GPU</h3>
+      </div>
+
+      <form action="" method="POST">
+        <div class="content">
+          <div class="row">
+
+            <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12">
+
+              <div class="card <?php if($_SESSION['themeColor'] == "dark-edition") { echo "card-dark"; } ?>">
+                <div class="card-header">
+                  <span class="card-title"></span>
+                </div>
+                <div class="card-body">
+
+                  <div class="table-responsive">
+                    <table class="table">
+                      <thead class="text-none">
+                        <th>Virtual GPUs [mdev]</th>
+                      </thead>
+                      <tbody>
+                    <!-- start project list -->
+                    <?php
+                    $result = shell_exec('cd /var/www/html/arclight/gpubinder && ./nvidia-dev-ctl.py list-mdev --output-all');
+
+                      echo "<tr>" .
+                        "<pre><td><strong>{$result}</strong></td></pre>" .
+                        "</tr>";
+                    
+                    echo "</tbody></table>";
+                    ?>
+                  </div>
+                </div>
+              </div>
+
+            </div>
+          </div>
+        </div>
+      </form>
+    </main>
+  </div> 
+</div> <!-- end content of virtual GPUs-->
+
+
+
+
