b5ba745fd763e4bf1f9c06d6bc7204a9c13cdc17
s4nfs <zyzzaestheticsgods@gmail.com>
2021-09-11 14:22:55 +0530

    added remove mdev option

--- pages/domain/vgputestbinder.php
+++ pages/domain/vgputestbinder.php
@@ -23,7 +23,9 @@
 if (isset($_POST['action'])) {
   $_SESSION['action'] = $_POST['action'];
   $_SESSION['pciaddr'] = clean_input($_POST['pciaddr']);
-  $_SESSION['mdevtype'] = clean_input($_POST['mdevtype']);
+  $_SESSION['mdevtype'] = $_POST['mdevtype'];
+  $_SESSION['UUID'] = $_POST['UUID'];
+  $_SESSION['domain_name'] = $_POST['domain_name'];
   unset($_POST);
   header("Location: ".$_SERVER['PHP_SELF']);
   exit;
@@ -57,6 +59,7 @@ domain_name varchar(255),
 action varchar(255),
 mdevtype varchar(255),
 createdmdev varchar(255),
+UUID varchar(255),
 dt DATETIME)";
 $tablesql = mysqli_query($conn, $sql);
 
@@ -65,25 +68,25 @@ $tablesql = mysqli_query($conn, $sql);
 // -------------------------------------CREATE MDEV GPU-------------------------------------
 // usage: nvidia-dev-ctl.py create-mdev [-n] PCI_ADDRESS, MDEV_TYPE_OR_NAME
 
-if ($action == "createmdev"){
-  $domain_name = $_SESSION['domain_name']; 
+if ($action == "createmdev"){ 
   $pciaddr = $_SESSION['pciaddr'];
   $mdevtype = $_SESSION['mdevtype'];
 
 
-  $createdmdev = shell_exec("cd /var/www/html/arclight/gpubinder && sudo ./nvidia-dev-ctl.py create-mdev '".$pciaddr."' '".$mdevtype."'");
-// if variable $createdmdev gives permission error
-// 1.Edit your sudoers file
-// nano /etc/sudoers
+  $createddmdev = shell_exec("cd /var/www/html/arclight/gpubinder && sudo ./nvidia-dev-ctl.py create-mdev '".$pciaddr."' '".$mdevtype."'");
+  $createdmdev = chop($createddmdev); //chop() function removes whitespaces or other predefined characters from the right end of a string. 
+  
+  // if variable $createdmdev gives permission error
+  // 1.Edit your sudoers file
+  // nano /etc/sudoers
 
-// 2.Put this line
-// www-data ALL=(ALL) NOPASSWD: ALL
-// ----------------------------------------------------------------------------------------
+  // 2.Put this line
+  // www-data ALL=(ALL) NOPASSWD: ALL
+  // ----------------------------------------------------------------------------------------
 
 
 if ($createdmdev){
-
-  $sql = "INSERT INTO arclight_vgpu (userid, domain_name, action, mdevtype, createdmdev, dt) VALUES('$userid', '$domain_name', '$action', '$mdevtype', '$createdmdev', current_timestamp());"; 
+  $sql = "INSERT INTO arclight_vgpu (userid, action, mdevtype, createdmdev, dt) VALUES('$userid', '$action', '$mdevtype', '$createdmdev', current_timestamp());"; 
   $inserttablesql = mysqli_query($conn, $sql);
 
   if(!$inserttablesql)
@@ -140,31 +143,273 @@ if ($createdmdev){
 </form>
 
 <!-------------------------------------ATTACH MDEV GPU------------------------------------->
+<!--usage: nvidia-dev-ctl.py attach-mdev [-h] [--virsh-trials N]
+                                     [--virsh-delay SECONDS] [-c URL]
+                                     [--hotplug] [--restart] [-n]
+                                     UUID DOMAIN
+
+positional arguments:
+  UUID                  UUID of the mdev device to attach
+  DOMAIN                domain name, id or uuid
+
+optional arguments:
+  -h, --help            show this help message and exit
+  --virsh-trials N      number of trials if waiting for virsh
+  --virsh-delay SECONDS
+                        delay time in seconds between trials if waiting for
+                        virsh
+  -c URL, --connection URL
+                        virsh connection URL
+  --hotplug             affect the running domain and keep changes after
+                        reboot
+  --restart             shutdown and reboot the domain after the changes are
+                        made
+  -n, --dry-run         Do everything except actually make changes -->
+
+<?php
+
+if ($action == "attachmdev"){ 
+  $UUID = $_SESSION['UUID'];
+  $domain_name = $_SESSION['domain_name'];
+  $optionalarguments = $_SESSION['optionalarguments'];
+  $UUID = $_SESSION['UUID'];
+
+
+  $attachmdev = shell_exec("cd /var/www/html/arclight/gpubinder && sudo ./nvidia-dev-ctl.py attach-mdev '".$optionalarguments."' '".$UUID."' '".$domain_name."'");
+
+  if ($attachmdev){
+
+    $sql = "INSERT INTO arclight_vgpu (userid, domain_name, action, UUID, dt) VALUES('$userid', '$domain_name', '$action', '$UUID', current_timestamp());"; 
+    $inserttable2sql = mysqli_query($conn, $sql);
+  
+    if(!$inserttable2sql)
+      {
+        // //     echo "Inserted into databse";
+        // }
+        // else{
+          echo("Error description: " . mysqli_error($conn));
+      }
+  
+  }
+  }
+  ?>
 <form class="row gx-3 gy-2 align-items-center" id="attachmdev" name="attachmdev" role="form" action="" method="post">
-  <div class="col-sm-3">
-    <label class="visually-hidden" for="specificSizeInputName">PCI Address</label>
-    <input type="text" class="form-control" id="pciaddr" name="pciaddr" placeholder="PCI Address">
-  </div>
-  <div class="col-sm-3">
-    <label class="visually-hidden" for="optionalarguments">Optional Arguments</label>
-    <select class="form-select" id="optionalarguments" name="optionalarguments">
-      <option selected>Choose...</option>
-      <option value="1">Hotplug</option>
-      <option value="2">Restart</option>
-      <option value="3">Dry Run</option>
-    </select>
-  </div>
   <div class="col-sm-3">
     <label class="visually-hidden" for="UUID">UUID</label>
     <input type="text" class="form-control" id="UUID" name="UUID" placeholder="UUID">
   </div>
   <div class="col-sm-3">
-    <label class="visually-hidden" for="domainname">Domain</label>
-    <input type="text" class="form-control" id="domainname" name="domainname" placeholder="Domain Name">
+    <label class="visually-hidden" for="domain_name">Domain</label>
+    <input type="text" class="form-control" id="domain_name" name="domain_name" placeholder="Domain Name">
   </div>
+  <div class="col-sm-3">
+    <label class="visually-hidden" for="optionalarguments">Optional Arguments</label>
+    <select class="form-select" id="optionalarguments" name="optionalarguments">
+      <option selected>Choose...</option>
+      <option value="">None</option>
+      <option value="--hotplug">Hotplug</option>
+      <option value="--restart">Restart</option>
+      <option value="-n">Dry Run</option>
+    </select>
+  </div>
+  <!-- <input type="hidden" name="action" value="attachmdev"> -->
   <div class="col-auto">
     <button type="Attach" class="btn btn-primary">Attach Virtual GPU</button>
   </div>
 </form>
-</body>
-</html>
+
+
+
+
+
+
+
+<!-------------------------------------Remove MDEV GPU------------------------------------->
+<?php
+$sql = "SELECT createdmdev, mdevtype FROM arclight_vgpu WHERE action = 'createmdev' AND userid = '$userid';";
+$result = $conn->query($sql);
+
+// if ($action == "removemdev"){ 
+//   $selectmdev = $_SESSION['selectmdev'];
+//   echo $selectmdev;
+
+  // $removedmdev = shell_exec("cd /var/www/html/arclight/gpubinder && sudo ./nvidia-dev-ctl.py remove-mdev '".$optionalarguments."'");
+  // if(!$removemdev)
+  // {
+  //   // //     echo "Inserted into databse";
+  //   // }
+  //   // else{
+  //       }
+  
+
+?>
+ 
+  <style>  
+    .container {  
+      max-width: 400px;  
+      margin: 60px auto;  
+      text-align: center;  
+    }  
+    input[type="submit"] {  
+      margin-bottom: 25px;  
+    }  
+    .select-block {  
+      width: 350px;  
+      margin: 100px auto 40px;  
+      position: relative;  
+    }  
+    select {  
+      width: 100%;  
+      height: 50px;  
+      font-size: 100%;  
+      font-weight: bold;  
+      cursor: pointer;  
+      border-radius: 0;  
+      background-color: #1A33FF;  
+      border: none;  
+      border: 2px solid #1A33FF;  
+      border-radius: 4px;  
+      color: white;  
+      appearance: none;  
+      padding: 8px 38px 10px 18px;  
+      -webkit-appearance: none;  
+      -moz-appearance: none;  
+      transition: color 0.3s ease, background-color 0.3s ease, border-bottom-color 0.3s ease;  
+    }  
+      select::-ms-expand {  
+      display: none;  
+    }  
+    /* .selectIcon {  
+      top: 7px;  
+      right: 15px;  
+      width: 30px;  
+      height: 36px;  
+      padding-left: 5px;  
+      pointer-events: none;  
+      position: absolute;  
+      transition: background-color 0.3s ease, border-color 0.3s ease;  
+    }   */
+    /* .selectIcon svg.icon {  
+      transition: fill 0.3s ease;  
+      fill: white;  
+    }   */
+    select:hover {  
+      color: #000000;  
+      background-color: white;  
+    }  
+select:focus {  
+      color: #000000;  
+      background-color: white;  
+    }  
+    select:hover~.selectIcon  
+     {  
+      background-color: white;  
+    }  
+    select:focus~.selectIcon {  
+      background-color: white;  
+    }  
+    select:hover~.selectIcon svg.icon  
+    {  
+      fill: #1A33FF;  
+    }  
+  select:focus~.selectIcon svg.icon {  
+      fill: #1A33FF;  
+    }  
+h2 {  
+ font-style: italic;  
+font-family: "Playfair Display","Bookman",serif;  
+ color: #999;   
+letter-spacing: -0.005em;   
+word-spacing:1px;  
+font-size: 1.75em;  
+font-weight: bold;  
+  }  
+h1 {  
+ font-style: italic;  
+ font-family: "Playfair Display","Bookman",serif;  
+ color: #999;   
+letter-spacing: -0.005em;   
+word-spacing: 1px;  
+ font-size: 2.75em;  
+  font-weight: bold;  
+  }  
+input[type=submit] {  
+  border: 3px solid;  
+  border-radius: 2px;  
+  color: ;  
+  display: block;  
+  font-size: 1em;  
+  font-weight: bold;  
+  margin: 1em auto;  
+  padding: 1em 4em;  
+  position: relative;  
+  text-transform: uppercase;  
+}  
+input[type=submit]::before,  
+input[type=submit]::after {  
+  background: #fff;  
+  content: '';  
+  position: absolute;  
+  z-index: -1;  
+}  
+input[type=submit]:hover {  
+  color: #1A33FF;  
+}  
+  </style>  
+ 
+  <div class="container mt-5">  
+
+<?php
+    if (mysqli_num_rows($result) > 0) {
+?>
+    <form action="" method="post">  
+      <select name="removemdev">  
+    <!-- <select class="form-select" id="selectmdev" name="selectmdev"> -->
+        <option value = "" selected> Select option </option>  
+    <?php
+$i=0;
+while($DB_ROW = mysqli_fetch_array($result)) {
+    ?>
+        <option value="<?php echo $DB_ROW["createdmdev"];?>"><?php echo $DB_ROW["mdevtype"];?>  (<?php echo $DB_ROW["createdmdev"];?>)</option>
+      <?php
+$i++;
+}
+    }
+    else{
+        echo "No device found";
+}
+?>
+    </select> 
+      <!-- <div class="selectIcon">  
+        <svg focusable="false" viewBox="0 0 104 128" width="25" height="35" class="icon">  
+          <path  
+            d="m2e1 95a9 9 0 0 1 -9 9 9 9 0 0 1 -9 -9 9 9 0 0 1 9 -9 9 9 0 0 1 9 9zm0-3e1a9 9 0 0 1 -9 9 9 9 0 0 1 -9 -9 9 9 0 0 1 9 -9 9 9 0 0 1 9 9zm0-3e1a9 9 0 0 1 -9 9 9 9 0 0 1 -9 -9 9 9 0 0 1 9 -9 9 9 0 0 1 9 9zm14 55h68v1e1h-68zm0-3e1h68v1e1h-68zm0-3e1h68v1e1h-68z">  
+          </path>  
+        </svg>  
+      </div>   -->
+      <br> <br> <input type = "submit" name = "submit" value = "submit">  
+    </form>  
+    <?php  
+        if(isset($_POST['submit'])){  
+        if(!empty($_POST['removemdev'])) {  
+            // $selected = $_POST['removemdev'];  
+            $selected = clean_input($_POST['removemdev']);
+            $removeddmdev = shell_exec("cd /var/www/html/arclight/gpubinder && sudo ./nvidia-dev-ctl.py remove-mdev '".$selected."'");
+
+        $rsql = "DELETE FROM arclight_vgpu WHERE createdmdev = '$selected';";
+        $result = $conn->query($rsql);
+
+        echo 'Removed MDEV ID:'  . $selected;
+
+    }
+     else {
+        echo 'Please select the value.'  . $selected;
+    }
+
+        } 
+        
+        
+    ?>  
+  </div>  
+</body>  
+</html> 
