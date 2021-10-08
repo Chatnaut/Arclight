a3235d85f3221ea41ab2cb823c2e420abb5725e9
s4nfs <zyzzaestheticsgods@gmail.com>
2021-10-08 22:22:39 +0530

    added js path for resize disk

--- pages/footer.php
+++ pages/footer.php
@@ -7,6 +7,8 @@
     <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery-slim.min.js"><\/script>')</script>
     <script src="../../assets/js/vendor/popper.min.js"></script>
     <script src="../../dist/js/bootstrap.min.js"></script>
+    <script src="../../dist/js/adddisk.js"></script>
+
      
     <!--  Notifications Plugin, full documentation here: http://bootstrap-notify.remabledesigns.com/    -->
     <script src="../../assets/js/plugins/bootstrap-notify.js"></script>
