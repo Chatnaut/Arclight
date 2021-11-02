ac6f359d322e5b877268e9f87b34a7ded0522985
s4nfs <zyzzaestheticsgods@gmail.com>
2021-11-02 22:42:25 +0530

    added modules for future updates

--- pages/navbar.php
+++ pages/navbar.php
@@ -150,7 +150,7 @@ require('../config/config.php');
                         </li>
                         <li class="nav-item">
                             <a class="nav-link" target="blank" href="../swmp/index.php">
-                            <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" aria-hidden="true" focusable="false" width="1em" height="1em" style="-ms-transform: rotate(360deg); -webkit-transform: rotate(360deg); transform: rotate(360deg);" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24"><path d="M9 19a1 1 0 0 0 .38-.08a1.15 1.15 0 0 0 .33-.21a1 1 0 0 0 .12-.16a.56.56 0 0 0 .09-.17a.64.64 0 0 0 .08-.18a1.36 1.36 0 0 0 0-.2a1 1 0 0 0-.08-.38a.9.9 0 0 0-.54-.54A1 1 0 0 0 8.8 17l-.18.06a.56.56 0 0 0-.17.09a1 1 0 0 0-.16.12a1 1 0 0 0-.21.33A1 1 0 0 0 8 18a1 1 0 0 0 1 1zm-3.71-.29a1.15 1.15 0 0 0 .33.21A1 1 0 0 0 6 19h.19a.6.6 0 0 0 .19-.06a.76.76 0 0 0 .18-.09l.15-.12a1.15 1.15 0 0 0 .21-.33A.84.84 0 0 0 7 18a1.36 1.36 0 0 0 0-.2a.64.64 0 0 0-.06-.18a.56.56 0 0 0-.09-.17a1 1 0 0 0-.12-.16a1 1 0 0 0-1.09-.21a1 1 0 0 0-.33.21a1 1 0 0 0-.12.16a.56.56 0 0 0-.09.17a.64.64 0 0 0-.1.18a1.36 1.36 0 0 0 0 .2a1 1 0 0 0 .08.38a1.15 1.15 0 0 0 .21.33zM19 2H5a3 3 0 0 0-3 3v14a3 3 0 0 0 3 3h14a3 3 0 0 0 3-3V5a3 3 0 0 0-3-3zm1 17a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1v-3h16zm0-5H4v-4h4a1 1 0 0 0 .71-.29L10 8.46l2.8 3.2a1 1 0 0 0 .72.34a1 1 0 0 0 .71-.29L15.91 10H20zm0-6h-4.5a1 1 0 0 0-.71.29l-1.24 1.25l-2.8-3.2a1 1 0 0 0-1.46 0L7.59 8H4V5a1 1 0 0 1 1-1h14a1 1 0 0 1 1 1z" fill="#626262"/><rect x="0" y="0" width="24" height="24" fill="rgba(0, 0, 0, 0)" /></svg>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-activity" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M6 2a.5.5 0 0 1 .47.33L10 12.036l1.53-4.208A.5.5 0 0 1 12 7.5h3.5a.5.5 0 0 1 0 1h-3.15l-1.88 5.17a.5.5 0 0 1-.94 0L6 3.964 4.47 8.171A.5.5 0 0 1 4 8.5H.5a.5.5 0 0 1 0-1h3.15l1.88-5.17A.5.5 0 0 1 6 2Z"></path></svg>
                                 Monitoring
                             </a>
                         </li>
@@ -159,10 +159,19 @@ require('../config/config.php');
                             <?php if($_SESSION['roles'] == "Enterprise") { ?>                        
                             <a class="nav-link" href="../gpu/gpubinder.php"> <?php } else { ?>
                             <a class="nav-link" href="../gpu/gpubinder_profiles.php"> <?php } ?>
-                            <span data-feather="link-2"></span>
+                            <!-- <span data-feather="link-2"></span> -->
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-gpu-card" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M0 1.5A.5.5 0 0 1 .5 1h1a.5.5 0 0 1 .5.5V4h13.5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-.5.5H2v2.5a.5.5 0 0 1-1 0V2H.5a.5.5 0 0 1-.5-.5Zm3 11h3.5v1a.5.5 0 0 1-.5.5H3.5a.5.5 0 0 1-.5-.5v-1Zm4 1v-1h4v1a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5ZM4 8a1.5 1.5 0 1 1 3 0 1.5 1.5 0 0 1-3 0Zm1.5-2.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5Zm6 1a1.5 1.5 0 1 0 0 3 1.5 1.5 0 0 0 0-3ZM9 8a2.5 2.5 0 1 1 5 0 2.5 2.5 0 0 1-5 0Z"></path></svg>
                                 GPU
                             </a>
                         </li>
+                        <li class="nav-item">
+
+                            <?php if($_SESSION['roles'] == "Enterprise") { ?>                        
+                            <a class="nav-link" href="../modules/update_modules.php"> 
+                            <span><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-box-seam" viewBox="0 0 16 16"><path d="M8.186 1.113a.5.5 0 0 0-.372 0L1.846 3.5l2.404.961L10.404 2l-2.218-.887zm3.564 1.426L5.596 5 8 5.961 14.154 3.5l-2.404-.961zm3.25 1.7-6.5 2.6v7.922l6.5-2.6V4.24zM7.5 14.762V6.838L1 4.239v7.923l6.5 2.6zM7.443.184a1.5 1.5 0 0 1 1.114 0l7.129 2.852A.5.5 0 0 1 16 3.5v8.662a1 1 0 0 1-.629.928l-7.185 2.874a.5.5 0 0 1-.372 0L.63 13.09a1 1 0 0 1-.63-.928V3.5a.5.5 0 0 1 .314-.464L7.443.184z"></path></svg></span>
+                                Modules
+                            </a> <?php } ?>
+                            </li>                        
                         
                     </ul>
                 </li>
