f526b483f02393941498ab230b287e6b8cb5c864
S4nfs <zyzzaestheticsgods@gmail.com>
2022-06-29 00:05:25 +0530

    fontawesome cdn

--- pages/header.php
+++ pages/header.php
@@ -23,6 +23,8 @@
   <!-- Btn Main and OScard Styles -->
   <link href="../../dist/css/buttons.css" rel="stylesheet">
   <link href="../../assets/css/oscard.css" rel="stylesheet">
+  <!-- fontawesome icons-->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
 
 <?php
