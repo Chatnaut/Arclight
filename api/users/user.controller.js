d90637e40a399dabc56fffe3c08240dafd46a1fc
S4nfs <zyzzaestheticsgods@gmail.com>
2022-07-02 22:20:31 +0530

    added controller for update and delete instance

--- api/users/user.controller.js
+++ api/users/user.controller.js
@@ -1,4 +1,4 @@
-const { create, getInstance, getUsers, updateUser, deleteUser, getUserByUserEmail, createInstance, getarcUsers, getAUC } = require("./user.service");
+const { create, getInstance, getUsers, updateUser, deleteUser, getUserByUserEmail, createInstance, updateUserInstance, deleteUserInstance, getarcUsers, getAUC } = require("./user.service");
 const { genSaltSync, hashSync, compareSync } = require("bcrypt"); //compareSync compare passwords to match
 const { get } = require("./user.router");
 const { sign } = require("jsonwebtoken");
@@ -55,7 +55,30 @@ module.exports = {
                 data: results
             });
         });
-    }, getUsers: (req, res) => {
+    },
+    updateInstance: (req, res) => {
+        const body = req.body;
+        updateUserInstance(body, (err, results) => {
+            if (err) {
+                console.log(err);
+                return res.status(500).json({
+                    success: 0,
+                    message: "Unable to update instance"
+                });
+            } if(results.affectedRows === 0) {
+                return res.status(404).json({
+                    success: 0,
+                    message: "Instance Not Found"
+                });
+            }
+            return res.status(200).json({
+                success: 1,
+                data: results
+            });
+        });
+    },
+
+    getUsers: (req, res) => {
         getUsers((err, results) => {
             if (err) {
                 console.log(err);
@@ -92,8 +115,9 @@ module.exports = {
                 message: "Database Updated"
             });
         });
-    }, deleteUser: (req, res) => {
-        const data = req.body;
+    },
+     deleteUser: (req, res) => {
+        const data = req.params;
         deleteUser(data, (err, results) => {
             if (err) {
                 console.log(err);
@@ -105,10 +129,29 @@ module.exports = {
                     message: "Record not found"
                 })
             }
-
             return res.json({
                 success: 1,
                 message: "User Deleted Successfully"
+            });
+        });
+    },
+
+    deleteinstance: (req, res) => {
+        const data = req.body;
+        deleteUserInstance(data, (err, results) => {
+            if (err) {
+                console.log(err);
+                return;
+            }
+            if(results.affectedRows === 0) {
+                return res.status(404).json({
+                    success: 0,
+                    message: "Record not found"
+                })
+            }
+            return res.status(200).json({
+                success: 1,
+                message: "Instance Deleted Successfully"
             });
         });
     },
@@ -137,7 +180,10 @@ module.exports = {
         const body = req.body; //user pass email and password
         getUserByUserEmail(body.email, (err, results) => {
             if (err) {
-                console.log(err);
+                return res.json({
+                    success: 0,
+                    message: "Database connection error"
+                });
             }
             if (!results) {
                 return res.json({
@@ -151,7 +197,7 @@ module.exports = {
             if (result) {
                 results.password = undefined;
                 //need authkey from pool
-                const jsontoken = sign({ result: results }, authKey, { expiresIn: "1h" });
+                const jsontoken = sign({ result: results }, authKey, { expiresIn: "5h" });
                 return res.json({
                     success: 1,
                     message: "Login Successfully",
