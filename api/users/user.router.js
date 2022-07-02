55fe35531457b372f2193785aa83a5eb068d105c
S4nfs <zyzzaestheticsgods@gmail.com>
2022-07-02 22:20:53 +0530

    endpoints for update and delete instance

--- api/users/user.router.js
+++ api/users/user.router.js
@@ -1,15 +1,19 @@
-const { createUser, getInstanceByUserId, getUsers, updateUser, deleteUser, login, createNewInstance, getArclightUsers, getArcUserConfig } = require("./user.controller")
+const { createUser, getInstanceByUserId, getUsers, updateUser, deleteUser, login, createNewInstance, updateInstance, deleteinstance, getArclightUsers, getArcUserConfig } = require("./user.controller")
 const router = require("express").Router(); // create a router instance
 const { checkToken } = require("../../auth/token_validation");
 
+//auth routes
 router.post("/", createUser);
 router.get("/", getUsers);
-router.get("/arcuser/:id", getInstanceByUserId); //changed route path to not conflict with other routes
-router.patch("/", checkToken, updateUser);
-router.delete("/", checkToken, deleteUser);
+router.patch("/", updateUser);
+// router.delete("/:id", deleteUser);
 router.post("/login", login);
-router.post("/createinstance", checkToken, createNewInstance);
 router.get("/a", checkToken, getArclightUsers);
+//arc routes
+router.get("/arcuser/:id", getInstanceByUserId); //changed route path to not conflict with other routes
+router.post("/createinstance", checkToken, createNewInstance);
+router.patch("/updateinstance", updateInstance);
+router.delete("/deleteinstance", deleteinstance);
 router.get("/auc/:email", checkToken, getArcUserConfig);
 
 module.exports = router;
