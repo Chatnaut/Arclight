3721f6a29f8bcc9d1816f0e72f555a7e4e6d6291
S4nfs <zyzzaestheticsgods@gmail.com>
2022-07-18 12:49:43 +0530

    formatted

--- pages/footer.php
+++ pages/footer.php
@@ -1,53 +1,54 @@
-
-
-    <!-- Bootstrap core JavaScript
+<!-- Bootstrap core JavaScript
     ================================================== -->
-    <!-- Placed at the end of the document so the pages load faster -->
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-    <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery-slim.min.js"><\/script>')</script>
-    <script src="../../assets/js/vendor/popper.min.js"></script>
-    <script src="../../dist/js/bootstrap.min.js"></script>
-     
-    <!--  Notifications Plugin, full documentation here: http://bootstrap-notify.remabledesigns.com/    -->
-    <script src="../../assets/js/plugins/bootstrap-notify.js"></script>
+<!-- Placed at the end of the document so the pages load faster -->
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script>
+  window.jQuery || document.write('<script src="../../assets/js/vendor/jquery-slim.min.js"><\/script>')
+</script>
+<script src="../../assets/js/vendor/popper.min.js"></script>
+<script src="../../dist/js/bootstrap.min.js"></script>
 
-    <!-- Icons -->
-    <script src="https://unpkg.com/feather-icons/dist/feather.min.js"></script>
-    <script>
-      feather.replace()
-    </script>
+<!--  Notifications Plugin, full documentation here: http://bootstrap-notify.remabledesigns.com/    -->
+<script src="../../assets/js/plugins/bootstrap-notify.js"></script>
 
-    <script>
-      window.onload =  function() {
-        <?php
-          if ($notification) {
-            echo "showNotification(\"top\",\"right\",\"$notification\");";
-          }
-        ?>
+<!-- Icons -->
+<script src="https://unpkg.com/feather-icons/dist/feather.min.js"></script>
+<script>
+  feather.replace()
+</script>
+
+<script>
+  window.onload = function() {
+    <?php
+    if ($notification) {
+      echo "showNotification(\"top\",\"right\",\"$notification\");";
+    }
+    ?>
+  }
+
+  function showNotification(from, align, text) {
+    color = 'warning';
+    $.notify({
+      icon: "",
+      message: text
+    }, {
+      type: color,
+      delay: 9000,
+      timer: 1000,
+      placement: {
+        from: from,
+        align: align
       }
-
-      function showNotification(from, align, text){
-        color = 'warning';
-        $.notify({
-            icon: "",
-            message: text
-          },{
-              type: color,
-              delay: 9000,
-              timer: 1000,
-              placement: {
-                  from: from,
-                  align: align
-              }
-          });
-      }
-    </script>
+    });
+  }
+</script>
 
 <!-- replaceState method of JQuery to prevent duplication of data submission due to post back -->
-    <script>
-      if ( window.history.replaceState ) {
-        window.history.replaceState( null, null, window.location.href );
-      }
-    </script>
-  </body>
+<script>
+  if (window.history.replaceState) {
+    window.history.replaceState(null, null, window.location.href);
+  }
+</script>
+</body>
+
 </html>
