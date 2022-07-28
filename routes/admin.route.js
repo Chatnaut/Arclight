cace6135df8537f1df8d96eda4b0020044a7d535
S4nfs <zyzzaestheticsgods@gmail.com>
2022-07-28 22:25:02 +0530

    enterpise user

--- routes/admin.route.js
+++ routes/admin.route.js
@@ -2,15 +2,15 @@ const {User} = require('../models/user.model')
 const router = require('express').Router();
 const mongoose = require('mongoose');
 const {roles} = require('../utils/constants')
-// router.get('/users', async (req, res, next) => {
-//     try {
-//         const users = await User.find();
-//         // res.send(users)
-//         res.render('manage-users', { users })
-//     } catch (error) {
-//         next(error)
-//     }
-// })
+router.get('/users', async (req, res, next) => {
+    try {
+        const users = await User.find();
+        // res.send(users)
+        res.render('manage-users', { users })
+    } catch (error) {
+        next(error)
+    }
+})
 
 
 // update roles
--- utils/constants.js
+++ utils/constants.js
@@ -1,5 +1,6 @@
 module.exports= {
     roles: {
+        enterprise: "ENTERPRISE",
         admin: "ADMIN",
         moderator: "MODERATOR",
         client: "CLIENT",
