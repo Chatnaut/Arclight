1290957fe0104302af50e2831d8e7abc9de1b206
S4nfs <zyzzaestheticsgods@gmail.com>
2022-07-29 21:50:26 +0530

    seperate profile route

---
+++ routes/profile.route.js
@@ -0,0 +1,13 @@
+router.get('/profile', async(req,res,next) => {
+    console.log(req.user)
+    const person = req.user
+    // res.render('profile', {person});
+    res.status(200).json({
+        success: 1,
+        message: "Profile retrieved successfully",
+        result: person
+    });
+
+})
+
+module.exports = router;
